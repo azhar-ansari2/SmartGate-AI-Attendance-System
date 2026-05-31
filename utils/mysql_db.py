3caacb50754c3809dacc6e5fc2fff746e78e9cb9
Md Azhar Ansari <azharansari9148@gmail.com>
2026-05-31 14:47:24 +0530

    Configure Railway MySQL

--- utils/mysql_db.py
+++ utils/mysql_db.py
@@ -3,9 +3,9 @@ import streamlit as st
 
 def get_connection():
     return mysql.connector.connect(
-        host=st.secrets["${{RAILWAY_PRIVATE_DOMAIN}}"],
+        host=st.secrets["${{mysql.railway.internal}}"],
         port=int(st.secrets["3306"]),
         user=st.secrets["root"],
-        password=st.secrets["${{MYSQL_ROOT_PASSWORD}}"],
-        database=st.secrets["${{MYSQL_DATABASE}}"]
+        password=st.secrets["${{rqZeaMMMxmOUosrSrwEgOcUKyKUgJPggYoot}}"],
+        database=st.secrets["${{railway}}"]
     )
