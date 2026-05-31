8dcc5e3e2a27532318a8c1e3b040973010d4b3d5
Md Azhar Ansari <azharansari9148@gmail.com>
2026-05-31 14:06:22 +0530

    Add Railway database config

--- utils/mysql_db.py
+++ utils/mysql_db.py
@@ -1,9 +1,11 @@
 import mysql.connector
+import streamlit as st
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="smartgate"
+        host=st.secrets["${{RAILWAY_PRIVATE_DOMAIN}}"],
+        port=int(st.secrets["3306"]),
+        user=st.secrets["root"],
+        password=st.secrets["${{MYSQL_ROOT_PASSWORD}}"],
+        database=st.secrets["${{MYSQL_DATABASE}}"]
     )
